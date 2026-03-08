c1eac2127cd3294d7d28c4a1e428e6444c1251f6
Daniel-okwier <coderdani229@gmail.com>
2026-03-08 12:27:18 +0300

    update sidebar

--- client/src/layouts/Sidebar.jsx
+++ client/src/layouts/Sidebar.jsx
@@ -1,39 +1,46 @@
-import { NavLink } from "react-router-dom"
-import { LayoutDashboard, BookOpen, PenSquare, Settings } from "lucide-react"
+import { Link } from "react-router-dom"
 
-export default function Sidebar() {
-  const linkClass =
-    "flex items-center gap-3 px-4 py-2 rounded-lg text-sm transition hover:bg-muted"
+export default function Sidebar(){
 
-  return (
-    <aside className="hidden md:flex flex-col w-64 border-r border-border p-6 space-y-6">
-      
-      <div className="text-xl font-semibold">
+  return(
+
+    <aside className="w-64 h-screen border-r border-border p-6 hidden md:block">
+
+      <h2 className="font-diary text-xl mb-8">
         MyDiary
-      </div>
+      </h2>
 
-      <nav className="flex flex-col space-y-2">
-        <NavLink to="/dashboard" className={linkClass}>
-          <LayoutDashboard size={18} />
+      <nav className="flex flex-col gap-4">
+
+        <Link to="/dashboard" className="hover:text-primary">
+         <LayoutDashboard size={18} />
           Dashboard
-        </NavLink>
+        </Link>
 
-        <NavLink to="/timeline" className={linkClass}>
-          <BookOpen size={18} />
+        <Link to="/create" className="hover:text-primary">
+         <PenSquare size={18} />
+        
+          Create Entry
+        </Link>
+
+        <Link to="/timeline" className="hover:text-primary">
+        <BookOpen size={18} />
           Timeline
-        </NavLink>
+        </Link>
 
-        <NavLink to="/create" className={linkClass}>
-          <PenSquare size={18} />
-          New Entry
-        </NavLink>
+        <Link to="/insights" className="hover:text-primary">
+<Cpu size={18} />          AI Insights
+        </Link>
 
-        <NavLink to="/settings" className={linkClass}>
-          <Settings size={18} />
+         <Link to="/settings" className="hover:text-primary">
+         <Settings size={18} />
           Settings
-        </NavLink>
+        </Link>
+
       </nav>
 
     </aside>
+
   )
+
 }
